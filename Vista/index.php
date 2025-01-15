d7ee711e9c6e4911ca9ab0e181c5ebf08ce8c264
2193076794 <2193076794@cua.uam.mx>
2025-01-14 19:22:12 -0600

    Fix js

--- Vista/index.php
+++ Vista/index.php
@@ -6,6 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script type='text/javascript' src='..\Controlador\Fachada_GUI.js'></script>
     <title>Cine UAM</title>
     <!--Bootstrap css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
@@ -224,7 +225,6 @@
      <!--Bootstrap Js -->
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
      integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-     <script type='text/javascript' src='..\Controlador\Fachada_GUI.js'></script>
     
     
 </body>
